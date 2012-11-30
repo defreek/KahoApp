413ba583f31c4b188f17141e38487d002428d319
detono <jonathan.tono@gmail.com>
2012-11-30 14:50:30 +0100

    agenda

--- KaHoSL/src/be/kahosl/agenda/AgendaFragment.java
+++ KaHoSL/src/be/kahosl/agenda/AgendaFragment.java
@@ -5,14 +5,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.CalendarView;
 import be.kahosl.R;
 import be.kahosl.TabFragment;
 
 public class AgendaFragment extends TabFragment {
+	private CalendarView cv;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		Log.wtf("Start module", "Agenda");
+		
+		cv = (CalendarView)container.findViewById(R.id.calCalendar);
+		
+		
 		
 		return inflater.inflate(R.layout.agenda_view, container, false);
 	}
@@ -21,4 +28,6 @@ public class AgendaFragment extends TabFragment {
 	public int getIcon(){
 		return R.drawable.ic_menu_agenda;
 	}
+	
+	
 }
